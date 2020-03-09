6f5f08cf7c4f18bccb2a49005566c127dd7539e2
Arhell <arhell333@gmail.com>
2020-03-09 22:43:17 +0200

    operators

---
+++ l4/index.php
@@ -0,0 +1,29 @@
+<?php
+
+//echo -5 + 3 * 5;
+//echo 5 / 3;
+//
+//echo 2 ** 3;
+//echo pow(2, 3);
+//$a = 5 + 3;
+//$a = 5;
+//$b = $a;
+//var_dump($a);
+//$a = 7;
+//var_dump($b);
+
+//$a = 5;
+//$b = &$a;
+//var_dump($a);
+//$a = 7;
+//var_dump($a);
+//var_dump($b);
+
+echo 5 + '3';
+
+?>
+
+
+
+
+
