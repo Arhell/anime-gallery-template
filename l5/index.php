c1f34240709ee96b133d44884a3698b8e7fe2ea1
Arhell <arhell333@gmail.com>
2020-03-10 00:08:36 +0200

    operators more

---
+++ l5/index.php
@@ -0,0 +1,42 @@
+<?php
+
+//$a = 5;
+//var_dump($a++);
+//var_dump($a);
+//
+//$b = 5;
+//var_dump(++$b);
+//var_dump($b);
+
+//$str1 = 'hello';
+//$str2 = ' world';
+//echo $str1 . $str2;
+
+$fruit = 'apple';
+//$fruits = "I have 2 {$fruit}s";
+//$fruits = "I have 2 $fruit" . "s";
+$fruits = "I have 2" . $fruit . "s";
+
+//$a = 5;
+//$a = $a + 2;
+//$a += 2;
+//$a -= 2;
+//$a *= 2;
+//var_dump($a);
+
+//$str1 = 'hello';
+//$str1 .= ' world';
+
+//var_dump($var); NULL
+//$var = '';
+//unset($var);
+$var = NULL;
+$var++;
+var_dump($var);
+
+?>
+
+
+
+
+
