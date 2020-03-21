e1c4a394c74f2f8e713b084517a87a7d176d3b9d
Arhell <arhell333@gmail.com>
2020-03-21 18:47:42 +0200

    while

---
+++ l7/index.php
@@ -0,0 +1,43 @@
+<?php
+
+error_reporting(-1);
+
+//$i = 1;
+//while ($i <= 10) {
+//  echo $i . '<br>';
+//  $i++; // $i = $i + 1; $i += 1
+//}
+
+//$b = 1;
+//echo "<table border=\"1\">\n";
+//while ($b <= 10) {
+//  echo "\t<tr>\n";
+//
+//  $n = 1;
+//  while ($n <= 3) {
+//    echo "\t\t<td>Row $b | Col - $n</td>\n";
+//    $n++;
+//  }
+//
+//  echo "\t</tr>\n";
+//  $b++;
+//}
+//echo '</table>';
+
+//$year = 1900;
+//echo '<select>' . "\n";
+//  while ($year <= 2015) {
+//    echo "\t<option value='$year'>$year</option>\n";
+//    $year++;
+//  }
+//echo '</select>';
+
+//$i = 11;
+//while ($i <= 10) {
+//  echo $i++;
+//}
+
+$i = 11;
+do{
+  echo $i++;
+}while($i <= 10);
