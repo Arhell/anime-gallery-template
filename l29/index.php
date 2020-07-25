efe43bc4e5fdb41731794e9765899d289879c097
Arhell <arhell333@gmail.com>
2020-07-25 19:16:09 +0300

    php7 2 spaceship

--- l29/index.php
+++ l29/index.php
@@ -2,3 +2,15 @@
 
 //$user = isset($_GET['user']) ? $_GET['user'] : 'quest';
 echo $user = $_GET['user'] ?? 'quest';
+$a = 1;
+$b = 2;
+
+//echo $a <=> $b;
+
+if(($a <=> $b) === 1) {
+  echo '$a > $b';
+} elseif (($a <=> $b) === -1) {
+  echo '$a < $b';
+} else {
+  echo '$a = $b';
+}
